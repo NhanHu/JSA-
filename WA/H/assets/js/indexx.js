afab4fe3e63bef7515dc3a04efa133eb4859ffa8
NhanHu <tommyshrimpys@gmail.com>
2022-08-16 16:06:56 +0700

    index_update

--- WA/H/assets/js/indexx.js
+++ WA/H/assets/js/indexx.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function(){
     $("#intro").click(function(){
-      $(".info").fadeIn();
+      $("#pic1").fadeIn(1000).fadeOut(4000)
+      $("#pic2").fadeIn(2000).fadeOut(3000)
+      $("#pic3").fadeIn(3000).fadeOut(2000)
+      $("#pic4").fadeIn(4000).fadeOut(1000)
+
+      
     });
   });
