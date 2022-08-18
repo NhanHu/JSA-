28737ed2d7ed10dd53e48beb01e57a81c610b000
NhanHu <tommyshrimpys@gmail.com>
2022-08-18 15:52:30 +0700

    updated_shop

--- WA/H/assets/js/indexx.js
+++ WA/H/assets/js/indexx.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
       $("#pic2").fadeIn(2000).fadeOut(3000)
       $("#pic3").fadeIn(3000).fadeOut(2000)
       $("#pic4").fadeIn(4000).fadeOut(1000)
+      $("#main_index_div").delay(5000).fadeIn(1000)
+      $("#intro").fadeOut()
+
       
 
       
