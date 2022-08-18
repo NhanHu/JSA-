ebb04eb2d14f6bf92f9d8c5b94682f3a4e57e0f1
NhanHu <tommyshrimpys@gmail.com>
2022-08-18 14:26:03 +0700

    Js

--- WA/H/assets/js/indexx.js
+++ WA/H/assets/js/indexx.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
       $("#pic2").fadeIn(2000).fadeOut(3000)
       $("#pic3").fadeIn(3000).fadeOut(2000)
       $("#pic4").fadeIn(4000).fadeOut(1000)
+      
 
       
     });
--- WA/H/assets/js/scriptt.js
+++ WA/H/assets/js/scriptt.js
@@ -1,42 +1,42 @@
 let products = [
     {
-        name: 'Pho',
-        title: 'Day la mon an co truyen cua Ha Noi va Viet Nam',
+        name: 'Pen 1A',
+        title: 'But 1A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'm'
     },
     {
-        name: 'Bun rieu',
-        title: 'Day la mon an tuyet voi va rat thich hop de di an voi nguoi yeu',
+        name: 'Pen 2A',
+        title: 'But 2A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'b'
     },
     {
-        name: 'Banh da cua',
-        title: 'Day la mot mon sieu ngon cua HaiPhong',
+        name: 'Pen 3A',
+        title: 'But 3A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'e'
     },
     {
-        name: 'Bun bo Hue',
-        title: 'Mon ngon cua Hue',
+        name: 'Pen 4A',
+        title: 'But 4A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'f'
     },
     {
-        name: 'Banh bot loc',
-        title: 'Mon ngon dac san cua Hue',
+        name: 'Pen 5A',
+        title: 'But 5A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'c'
     },
     {
-        name: 'Bun oc ',
-        title: 'Mot mon ngon va dinh cao vao mua dong',
+        name: 'Pen 6A',
+        title: 'But 6A',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
         img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
         id: 'a'
@@ -44,11 +44,12 @@ let products = [
 ]
 let items = document.querySelector('#first-div')
 for (i of products){
-    items.innerHTML += `<div class="small_div"><img src=${i.img}> <h4 id = ${products.id}>${i.name}</h4><div class = "button" onclick="onclicks('${i.id}')"><p>Bấm vào đây để mua</p></div></div>`
+    items.innerHTML += `<div class="small_div"><img src=${i.img}> <h4 id = ${products.id}>${i.name}</h4><div class = "button" onclick="onclicks('${i.img}, ${i.name}, ${i.des}')"><p>Bấm vào đây để mua</p></div></div>`
 }
 console.dir()
-localStorage.setItem("e", JSON.stringify(products))
-function onclicks(Value){
-    
-    let co = document.getElementById(Value).textContent
-    document.getElementById("second-div").innerHTML += `<p>${co}</p><br>`}
+
+function onclicks(img, name, des){
+        console.log(img)
+        console.log(name)
+        console.log(des)
+    }
