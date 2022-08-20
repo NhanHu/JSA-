195c8b2d46f7301d75b5d289ee794171ca3760f0
NhanHu <tommyshrimpys@gmail.com>
2022-08-20 08:45:04 +0700

    finished_index

--- WA/H/assets/js/scriptt.js
+++ WA/H/assets/js/scriptt.js
@@ -2,54 +2,55 @@ let products = [
     {
         name: 'Pen 1A',
         title: 'But 1A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'm'
+        des: '$40',
+        img: 'https://media.istockphoto.com/photos/pen-picture-id1059543698?k=20&m=1059543698&s=170667a&w=0&h=BKhT1Q-6CZHasjhpA4UR2O6fJ1COKUc5DiUIUYOIzAE=',
+        id: 'h1'
     },
     {
         name: 'Pen 2A',
         title: 'But 2A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'b'
+        des: '$20',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeYwtexH84MFt_FfS_T-6Z29D403f0ubL-CM4JChTFDQrW6JfskQTuILpTmvH-Ivb7r8s&usqp=CAU',
+        id: 'h2'
     },
     {
         name: 'Pen 3A',
         title: 'But 3A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'e'
+        des: '$39',
+        img: 'https://i.pinimg.com/originals/c0/16/d5/c016d599e4165520c60f006354e9ac43.jpg',
+        id: 'h3'
     },
     {
         name: 'Pen 4A',
         title: 'But 4A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'f'
+        des: '$12',
+        img: 'https://5scenter.in/wp-content/uploads/2021/04/pen.jpg',
+        id: 'h4'
     },
     {
         name: 'Pen 5A',
         title: 'But 5A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'c'
+        des: '$34',
+        img: 'https://m.media-amazon.com/images/I/711F6-eLS6L._AC_SS450_.jpg',
+        id: 'h5'
     },
     {
         name: 'Pen 6A',
         title: 'But 6A',
-        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae eaque consectetur voluptates amet rem cum commodi distinctio iure? Animi nulla repellat hic? Voluptatum totam iusto eaque deleniti officiis nemo?',
-        img: '/Users/tom/Desktop/WA/H/assets/img/hello.jpeg',
-        id: 'a'
+        des: '$1763223',
+        img: 'https://media.istockphoto.com/photos/pen-picture-id1059543698?k=20&m=1059543698&s=170667a&w=0&h=BKhT1Q-6CZHasjhpA4UR2O6fJ1COKUc5DiUIUYOIzAE=',
+        id: 'h6'
     }
 ]
 let items = document.querySelector('#first-div')
 for (i of products){
-    items.innerHTML += `<div class="small_div"><img src=${i.img}> <h4 id = ${products.id}>${i.name}</h4><div class = "button" onclick="onclicks('${i.img}, ${i.name}, ${i.des}')"><p>Bấm vào đây để mua</p></div></div>`
+    items.innerHTML += `<div class="small_div" id="${i.id}"><img src=${i.img}> <h4 id = ${products.id}>${i.name}</h4><div class = "button" onclick="onclicks('${i.img}', '${i.name}', '${i.des}')"><p>Click here to buy</p></div></div>`
 }
 console.dir()
 
 function onclicks(img, name, des){
-        console.log(img)
-        console.log(name)
-        console.log(des)
+    localStorage.setItem("img", JSON.stringify(img))
+    localStorage.setItem("name", JSON.stringify(name))
+    localStorage.setItem("des", JSON.stringify(des))
+
     }
