af842174ca55c1fed2fd7cd42e57e8536c507a01
NhanHu <tommyshrimpys@gmail.com>
2022-08-23 09:17:39 +0700

    helllo

---
+++ WA/H/assets/js/buy.js
@@ -0,0 +1,6 @@
+let Buy = {
+    name: `${JSON.parse(localStorage.getItem("name"))}`,
+    des: `${JSON.parse(localStorage.getItem("des"))}`,
+    img: `${JSON.parse(localStorage.getItem("img"))}`,
+}
+console.log(Buy.name)
