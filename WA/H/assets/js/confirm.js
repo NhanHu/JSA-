5b7759cc0aaab94efcbdec86bffd1b910a111934
NhanHu <tommyshrimpys@gmail.com>
2022-09-01 14:11:07 +0700

    Hello HelloH

--- WA/H/assets/js/confirm.js
+++ WA/H/assets/js/confirm.js
@@ -1,4 +1,4 @@
-d = 0
+let d = 1
 let a = document.querySelector('#nav')
 localStorage.setItem("Buy", JSON.stringify([]))
 a.innerHTML += `<a href = "/Users/tom/Desktop/JSA-/WA/H/assets/Confirm.html"id="fiffav">${JSON.parse(localStorage.getItem("yeah")).length}</a>`
@@ -7,13 +7,29 @@ localStorage.setItem("Buy", 0)
 let get_Item = JSON.parse(localStorage.getItem("yeah"))
 let hello = document.querySelector('#hehe')
 for (i of get_Item){
-  hello.innerHTML += `<div id = "${d}" class="small_div"><h1 id ="Name">${i}<h1 onclick="Add('${d}')">+</h1><h1>-</h1></h1></div>`
-  d++
+  hello.innerHTML += `<div id = "${i}" class="small_div"><h1 id ="Name">${i}<h1 onclick="Add('${i}')">+</h1><h1 onclick="Del('${i}')">-</h1></h1></div>`
 }
 
 function Add(v){
-    console.log(v)
+    
+    let get_Item = JSON.parse(localStorage.getItem("yeah"))
+    console.log(d)
+    hello.innerHTML += `<div id = "${v}" i"class="small_div"><h1 id ="Name">${v}<h1 onclick="Add('${v}')">+</h1><h1 onclick="Del('${i}')">-</h1></h1></div>`
+    get_Item.push(`${v}`)
+    console.log(get_Item)
+    localStorage.setItem("yeah", JSON.stringify(get_Item))
+    location.href = "/Users/tom/Desktop/JSA-/WA/H/assets/Confirm.html";
+
 }
+function Del(v){
+    const element = document.getElementById(`${v}`);
+    element.remove();
+    let get_Item = JSON.parse(localStorage.getItem("yeah"))
+    var Index = get_Item.indexOf(`${v}`);
+    get_Item.splice(Index, 1);
+    localStorage.setItem("yeah", JSON.stringify(get_Item))
+    location.href = "/Users/tom/Desktop/JSA-/WA/H/assets/Confirm.html";
+} 
 function RESET(){
     localStorage.setItem("yeah", JSON.stringify([]))
 }
